aaa46b9b5c3f3b2a14b15fe5247b462475d75c6f
htragan <htragan@crimson.ua.edu>
2022-04-29 16:15:59 -0500

    changed file names

--- client/scripts/customerAccount.js
+++ client/scripts/customerAccount.js
@@ -17,13 +17,29 @@ function getCustomerInfo() {
 
         json.forEach((customer) => {
             
+            console.log(customer.isLoggedIn);
+            
             if(customer.isLoggedIn == true)
             {
-                
-            }
-        });
+                var firstname = customer.cfname;
+                var lastname = customer.clname;
+                var username = customer.cusername;
+                var password = customer.cpassword;
+                var email = customer.cemail;
+                var phonenumber = customer.cphonenumber;
+                var businessname = customer.businessname;
 
-    
+                html += '<p><strong>First Name: ' + firstname + '</strong></p>';
+                html += '<p><strong>Last Name: ' + lastname + '</strong></p>';
+                html += '<p><strong>Username: ' + username + ' sqFt</strong></p>';
+                html += '<p><strong>Password: ' + password + ' sqFt</strong></p>';
+                html += '<p><strong>Password: ' + email + ' sqFt</strong></p>';
+                html += '<p><strong>Password: ' + phonenumber + ' sqFt</strong></p>';
+                html += '<p><strong>Password: ' + businessname + ' sqFt</strong></p>';
+            }
+
+        });
+        document.getElementById("info").innerHTML = html;
 
     }).catch(function(error){
         console.log(error);
